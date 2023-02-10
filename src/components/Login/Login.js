4f6093a4988a168a22bcf2f20a444b8cab579715
alistkov <antonlistkov@gmail.com>
2023-02-10 18:47:32 +0300

    video 115

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -19,26 +19,18 @@ const Login = (props) => {
     };
   }, [enteredPassword]);
 
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      console.log('Checking form validity!');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      );
-    }, 500);
-
-    return () => {
-      console.log('CLEANUP');
-      clearTimeout(timerId);
-    };
-  }, [enteredEmail, enteredPassword]);
-
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(
+      enteredEmail.includes('@') && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
