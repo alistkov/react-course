a7a63d5f0901e89c53d0afd3430cd41919f3780e
alistkov <antonlistkov@gmail.com>
2023-02-11 21:34:31 +0300

    video 156

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 const App = () => {
   const [paragraphIsVisible, setParagraphIsVisible] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('App running!');
 
   const toggleVisible = useCallback(() => {
-    setParagraphIsVisible((prevState) => !prevState);
-  }, []);
+    if (allowToggle) {
+      setParagraphIsVisible((prevState) => !prevState);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={paragraphIsVisible} />
+      <Button onClick={allowToggleHandler}>Allow toggling</Button>
       <Button onClick={toggleVisible}>Toggle paragraph</Button>
     </div>
   );
