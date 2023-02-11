86e3b605aa3a9a5dda262fa72cb2ad7a16a7c1c7
alistkov <antonlistkov@gmail.com>
2023-02-11 20:46:45 +0300

    video 152

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import Button from './components/UI/Button/Button';
+
 const App = () => {
-  return <h1>React - the complete guide</h1>;
+  const [paragraphIsVisible, setParagraphIsVisible] = useState(false);
+
+  console.log('App running!');
+
+  const toggleVisible = () => {
+    setParagraphIsVisible((prevState) => !prevState);
+  };
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      {paragraphIsVisible && <p>This is new!</p>}
+      <Button onClick={toggleVisible}>Toggle paragraph</Button>
+    </div>
+  );
 };
 
 export default App;
