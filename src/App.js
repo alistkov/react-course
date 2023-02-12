d448d31686f17484669ca1a201ed2609e277fbec
alistkov <antonlistkov@gmail.com>
2023-02-12 18:07:57 +0300

    video 177

--- src/App.js
+++ src/App.js
@@ -6,24 +6,16 @@ import './App.css';
 const App = () => {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films/')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movie) => ({
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        }));
-        console.log(transformedMovies);
-        setMovies(transformedMovies);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const fetchMoviesHandler = async () => {
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+    const transformedMovies = data.results.map((movie) => ({
+      id: movie.episode_id,
+      title: movie.title,
+      openingText: movie.opening_crawl,
+      releaseDate: movie.release_date,
+    }));
+    setMovies(transformedMovies);
   };
 
   return (
